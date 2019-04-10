9d06bf6084c1233beaaa52be31946b8940f36f6d
Nobot__ <31627752+Yatheon@users.noreply.github.com>
2019-04-10 10:07:54 +0200

    Main TCP Listen

--- src/Java/org/toggle/toggleio/core/Start.java
+++ src/Java/org/toggle/toggleio/core/Start.java
@@ -3,7 +3,17 @@ package org.toggle.toggleio.core;
 import org.toggle.toggleio.server.ToggleServer;
 
 public class Start {
+
     public static void main(String[] args) {
-        ToggleServer.start();
+        if (args.length > 0) {
+            try {
+                ToggleServer.start(Integer.parseInt(args[0]));
+            } catch (NumberFormatException e) {
+                System.out.println("Usage: Start 'port'");
+                return;
+            }
+
+        }
+        else ToggleServer.start();
     }
 }
--- src/Java/org/toggle/toggleio/server/ToggleServer.java
+++ src/Java/org/toggle/toggleio/server/ToggleServer.java
@@ -1,11 +1,58 @@
 package org.toggle.toggleio.server;
 
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.imageio.IIOException;
 
 public class ToggleServer {
     public static void start(){
-        DataOutputStream socket;
-        RequestHandler.handleRequest(socket);
+        int port = 80;
+        runtime(port);
+    }
+    public static void start(int port){
+       runtime(port);
+    }
+    public static void runtime(int port) {
+
+        String clientSentence;
+        ServerSocket welcomeSocket;
+        String clientLines = null;
+
+        try {
+            welcomeSocket = new ServerSocket(port);
+        } catch (Exception ex) {
+            System.out.println("Could not open Socket!");
+            return;
+        }
+        try {
+            while (true) {
+
+                Socket connectionSocket = welcomeSocket.accept();
+                BufferedReader fromClient =
+                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+                StringBuilder sentenceBuilder = new StringBuilder();
+                while ((clientLines = fromClient.readLine()) != null) {
+                    if (clientLines.isEmpty())
+                        break;
+                    sentenceBuilder.append(clientLines + "\n");
+                }
+
+                sentenceBuilder.setLength(sentenceBuilder.length() - 1);
+                clientSentence = sentenceBuilder.toString();
+
+                RequestHandler.handleRequest(connectionSocket, clientSentence);
+            }
+        }
+        catch (IOException ioe){
+            System.out.println("Something went wrong");
+            return;
+        }
+
+
     }
 
 }
