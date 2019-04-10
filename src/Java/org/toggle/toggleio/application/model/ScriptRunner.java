4001af9a41ff052d1b1f10d4e0586080397398a1
Michel Frog <m.quadria@gmail.com>
2019-04-10 15:16:14 +0200

    changed TelldusScript

--- src/Java/org/toggle/toggleio/application/model/ScriptRunner.java
+++ src/Java/org/toggle/toggleio/application/model/ScriptRunner.java
@@ -9,28 +9,12 @@ import java.io.IOException;
  * @version 1.0
  */
 public class ScriptRunner {
-/*
-    public static void main(String args[]) {
-        String script = "tdtool --on 2";
-        runScript(script);
-    }
-*/
+
     public static boolean runScript(String script) {
-        String isWindows = "win";
-        String isMac = "mac";
-        String isUnix = "nix";
 
         boolean success = true;
         try {
-            String OS = System.getProperty("os.name").toLowerCase();
-
-            if (OS.equals(isWindows)) {
-                Runtime.getRuntime().exec("powershell.exe " + script);
-            } else if (OS.equals(isMac)) {
-                Runtime.getRuntime().exec("/bin/bash " + script);
-            } else if (OS.equals(isUnix)) {
-                Runtime.getRuntime().exec("sh " + script);
-            }
+                Runtime.getRuntime().exec(script);
         } catch (IOException e) {
             e.printStackTrace();
             success = false;
