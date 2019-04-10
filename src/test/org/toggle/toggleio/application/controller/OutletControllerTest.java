c0df5505221924ad473096da6b57fe40171929ed
Marcus <mannils233@gmail.com>
2019-04-10 13:50:36 +0200

    fix?

--- src/test/org/toggle/toggleio/application/controller/OutletControllerTest.java
+++ src/test/org/toggle/toggleio/application/controller/OutletControllerTest.java
@@ -1,23 +1,9 @@
 package org.toggle.toggleio.application.controller;
-
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-<<<<<<< HEAD
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
-
-=======
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.io.IOException;
-import java.sql.SQLException;
 import org.toggle.toggleio.application.model.ScriptRunner;
 import org.toggle.toggleio.application.model.TelldusScripts;
->>>>>>> 0f09ef0842dc88009978907dde6e12da920fe349
 
 class OutletControllerTest {
 
