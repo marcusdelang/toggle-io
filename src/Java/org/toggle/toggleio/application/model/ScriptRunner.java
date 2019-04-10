56974e3e5287e80b874a94a0abf8dbfcaddc988c
Michel Frog <m.quadria@gmail.com>
2019-04-10 13:51:09 +0200

    first draft of ScriptRunner

--- src/Java/org/toggle/toggleio/application/model/ScriptRunner.java
+++ src/Java/org/toggle/toggleio/application/model/ScriptRunner.java
@@ -2,14 +2,36 @@ package org.toggle.toggleio.application.model;
 
 import java.io.IOException;
 
+/**
+ * The class is used to perform scripts through bash for respective operatingsystem.
+ *
+ * @author Michel Ouadria
+ * @version 1.0
+ */
 public class ScriptRunner {
-    public static boolean runScript(String script){
+/*
+    public static void main(String args[]) {
+        String script = "tdtool --on 2";
+        runScript(script);
+    }
+*/
+    public static boolean runScript(String script) {
+        String isWindows = "win";
+        String isMac = "mac";
+        String isUnix = "nix";
+
         boolean success = true;
         try {
-            Process process = Runtime.getRuntime().exec("powershell.exe " + script);
+            String OS = System.getProperty("os.name").toLowerCase();
 
-        }
-        catch (IOException e) {
+            if (OS.equals(isWindows)) {
+                Runtime.getRuntime().exec("powershell.exe " + script);
+            } else if (OS.equals(isMac)) {
+                Runtime.getRuntime().exec("/bin/bash " + script);
+            } else if (OS.equals(isUnix)) {
+                Runtime.getRuntime().exec("sh " + script);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
             success = false;
             return success;
