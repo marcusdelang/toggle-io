0f09ef0842dc88009978907dde6e12da920fe349
Marcus <mannils233@gmail.com>
2019-04-10 13:00:33 +0200

    fixed tests

--- src/test/org/toggle/toggleio/application/controller/OutletControllerTest.java
+++ src/test/org/toggle/toggleio/application/controller/OutletControllerTest.java
@@ -4,42 +4,43 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import org.toggle.toggleio.application.model.ScriptRunner;
+import org.toggle.toggleio.application.model.TelldusScripts;
 
 class OutletControllerTest {
 
-	private OutletController outletController;
-	private String script;
-	
-    @BeforeEach
-    void setUp() {
-    	//ScriptRunner scriptRunner = new ScriptRunner();        
-    	//TelldusScript telldusScript = new TelldusScript();
-    	outletController=new OutletController();
-        script=TelldusScript.on();
-    }
+  private OutletController outletController;
+  private String script;
+  private ScriptRunner scriptRunner;
 
-    @Test
-    void isAlive() {
-    	assertTrue(outletController.isAlive());
-    }
+  @BeforeEach
+  void setUp() {
+    //ScriptRunner scriptRunner = new ScriptRunner();
+    //TelldusScript telldusScript = new TelldusScript();
+    outletController = new OutletController();
+    scriptRunner = new ScriptRunner();
+    script = TelldusScripts.on();
+  }
 
-    @Test
-    void turnOn() {
-        assertThat(script, containsString("tdtool --on 2"));        
-        assertThat(scriptRunner.runScript(script),is(true));
-    }
+  @Test
+  void isAlive() {
+    assertTrue(outletController.isAlive());
+  }
 
-    @Test
-    void turnOff() {
-        assertThat(script, containsString("tdtool --off 2"));        
-        assertThat(scriptRunner.runScript(script,is(true));
-    }
-    
+  @Test
+  void turnOn() {
+    assertEquals("tdtool --on 2", script);
+    assertTrue(scriptRunner.runScript(script));
+  }
+
+  @Test
+  void turnOff() {
+    assertEquals(script, "tdtool --off 2");
+    assertEquals(scriptRunner.runScript(script), true);
+  }
+
 }
