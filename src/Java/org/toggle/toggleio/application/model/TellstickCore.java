eb16e92d5ce10a47a6198cb2554bf823126d28a1
Nobot__ <Gustav.bohlmark@gmail.com>
2019-04-29 13:42:46 +0200

    Update TellstickCore.java

--- src/Java/org/toggle/toggleio/application/model/TellstickCore.java
+++ src/Java/org/toggle/toggleio/application/model/TellstickCore.java
@@ -1,17 +1,32 @@
 package org.toggle.toggleio.application.model;
 
+
+import org.json.JSONObject;
+
 /**
  * Class that performs operations on the tellstick using the operating systems terminal and calling
  * the program tdtool
  */
 public class TellstickCore {
+
+  private JSONObject status;
+
+  public TellstickCore(){
+    status.append("status", "off");
+  }
+  public JSONObject getStatus() {
+    return status;
+  }
+
   /**
    * Turns device with id 1 on tellduscenter ON using connected tellstick and gives feedback on the success
    * of writing the command to a terminal, it does NOT return the the success of the command itself.
    * @return returns true if command was successfully executed in operating system terminal
    */
   public static boolean on(){
-    return  ScriptRunner.runScript(TelldusScripts.on());
+    boolean success = ScriptRunner.runScript(TelldusScripts.on());
+    status.
+    return success;
   }
   /**
    * Turns device with id 1 on tellduscenter OFF using connected tellstick and gives feedback on the success
@@ -19,7 +34,8 @@ public class TellstickCore {
    * @return returns true if command was successfully executed in operating system terminal
    */
   public static boolean off(){
-    return ScriptRunner.runScript(TelldusScripts.off());
+    boolean success = ScriptRunner.runScript(TelldusScripts.off());
+    return success;
   }
 
 }
