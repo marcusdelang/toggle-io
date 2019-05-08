1cc0d6b63021eb24f437e2e28441f699a1948452
Nobot_ <31627752+Yatheon@users.noreply.github.com>
2019-05-08 10:28:53 +0200

    Javadoc

--- src/Java/org/toggle/toggleio/server/JsonFile.java
+++ src/Java/org/toggle/toggleio/server/JsonFile.java
@@ -8,8 +8,16 @@ import org.json.simple.parser.ParseException;
 
 import java.io.*;
 
+/**
+ * This class handles writing and reading a file of type .json
+ */
 public class JsonFile {
-
+    /**
+     * Returns a json of type JSONObject if a file exist and contains correctly formated json
+     * @param filename of file that should be read from
+     * @return JSONObject
+     * @throws ParseException throws ParseException if file is not of json type or incorrectly formated
+     */
     public static JSONObject read(String filename) throws ParseException, IOException {
         JSONParser jsonParser = new JSONParser();
         try (FileReader reader = new FileReader(filename)){
@@ -24,12 +32,18 @@ public class JsonFile {
         }
 
     }
+
+    /**
+     * Writes the json object to a existing json file or creates a new file if none exist
+     * @param jsonObject to write in file
+     * @param filename of file to write to
+     * @throws IOException if
+     */
     public static void write(JSONObject jsonObject,String filename) throws IOException{
         try (FileWriter file = new FileWriter(filename)) {
 
             file.write(jsonObject.toJSONString());
             file.flush();
-
         } catch (IOException e) {
             throw e;
         }
